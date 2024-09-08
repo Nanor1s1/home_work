04ede09016318fc04c8b9100e46b8baeea93eaeb
Nanor1s1 <114857478+Nanor1s1@users.noreply.github.com>
2024-09-08 22:52:02 +0500

    Update home_work1.py

--- home_work1.py
+++ home_work1.py
@@ -6,11 +6,3 @@ if a==b:
 else:
     print("числа не равны")
 
-if b==0:
-    print("на 0 нельзя делить")
-else:
-    c = a%b
-    if c == 0:
-        print("число картно")
-    else:
-        print("не кратно")
