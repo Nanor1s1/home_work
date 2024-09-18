6d1d537a78f34742136d5552adc14dffc327ada1
Nanor1s1 <114857478+Nanor1s1@users.noreply.github.com>
2024-09-18 21:43:31 +0500

    Update home_work3.9.1.py

--- home_work3/home_work3.9.1.py
+++ home_work3/home_work3.9.1.py
@@ -1,20 +1,18 @@
 stipendia=0
+stipendia1=int(input("Enter the stipedia of: "))
 rashodi=0
+rashodi1=int(input("Enter the rashodi of: "))
 start=0
 months=int(input("Enter the number of months: "))
 
 # Calculate the amount of money after 12 months
 
 for i in range(months):
-    stipendia += 100
-    rashodi += 120
+    stipendia += stipendia1
+    rashodi += rashodi1
 start_sum=rashodi-stipendia
 
 print(f"The initial amount of money: {start_sum}")
 print(f"The stipendia of money: {stipendia}")
 print(f"The rashodi of money: {rashodi}")
-
-
-
-
 
