60e2cb321841b15cd7aeaa2d38be0fe73f8c08c6
Nanor1s1 <114857478+Nanor1s1@users.noreply.github.com>
2024-09-18 21:43:09 +0500

    Update home_work3.9.2.py

--- home_work3/home_work3.9.2.py
+++ home_work3/home_work3.9.2.py
@@ -1,17 +1,19 @@
 stipendia1=0
 stipendia2=0
+stipendiab=int(input("Enter the number of: "))
 rashodi1=rashodi2=0
+rashodib=int(input("Enter the number of: "))
 start=0
 months=int(input("Enter the number of months: "))
 
 for i in range(1,months,2):
-    stipendia1 += 100
-    rashodi1+=120
+    stipendia1 += stipendiab
+    rashodi1+=rashodib
     rashodi1*=1.03
 
 for i in range(0,months,2):
-    stipendia2  += 100
-    rashodi2+=120
+    stipendia2  += stipendiab
+    rashodi2+=rashodib
     rashodi2*=1.05
 
 stipendia=stipendia1+stipendia2
@@ -20,3 +22,4 @@ rashodi=rashodi2+rashodi1
 print(f"The stipendia of money: {stipendia}")
 print(f"The rashodi of money: {round(rashodi,2)}")
 print(f"The initial amount of money: {round(rashodi-stipendia,2)}")
+
